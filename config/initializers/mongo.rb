a52532cf80ef86d535446c62bc2be09a559972a3
Olivier BONNAURE <olivier@solisoft.net>
2011-12-07 09:10:57 +0100

    First Layout

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,3 +1,5 @@
-@connection = ReplSetConnection.new(['mongo1.mongood.com', 27017], ['mongo2.mongood.com', 27017], ['mongo3.mongood.com', 27017], :read_secondary => true)
-@connection.add_auth("bridgeo", "loth9ohlieNaeD1YahsiebeXaimohbaey3thoh3ooR", "queng3leNeeSiph4iejaeRawain4sheVohpieSeec7")
-@connection.apply_saved_authentication()
+$connection = Mongo::ReplSetConnection.new(['mongo1.mongood.com', 27017], ['mongo2.mongood.com', 27017], ['mongo3.mongood.com', 27017], :read => :secondary)
+$connection.add_auth("bridgeo", "loth9ohlieNaeD1YahsiebeXaimohbaey3thoh3ooR", "queng3leNeeSiph4iejaeRawain4sheVohpieSeec7")
+# TODO: We need to load another databases
+# One db per website
+$connection.apply_saved_authentication()
