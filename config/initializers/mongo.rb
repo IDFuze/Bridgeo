222c9a14f9723180d49ed5ed284871227723ca7b
Olivier BONNAURE <olivier@solisoft.net>
2011-12-11 18:21:51 +0100

    Mongo Loader

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,5 +1,15 @@
-$connection = Mongo::ReplSetConnection.new(['mongo1.mongood.com', 27017], ['mongo2.mongood.com', 27017], ['mongo3.mongood.com', 27017], :read => :secondary)
-$connection.add_auth("bridgeo", "loth9ohlieNaeD1YahsiebeXaimohbaey3thoh3ooR", "queng3leNeeSiph4iejaeRawain4sheVohpieSeec7")
-# TODO: We need to load another databases
-# One db per website
-$connection.apply_saved_authentication()
+$connection = nil
+
+config_file = "#{Rails.root}/config/mongo.yml"
+if File.exists? config_file 
+  m = YAML::load(IO.read(config_file))
+  if m[Rails.env]["server"].class == Array
+    $connection = Mongo::ReplSetConnection.new(*m[Rails.env]["server"], :read => :secondary)
+  else
+    $connection = Mongo::Connection.new(m[Rails.env]["server"], nil, :logger => Rails.env == "development" ? Rails.logger : nil)
+  end
+  unless m[Rails.env]["user"].to_s.blank?
+    $connection.add_auth(m[Rails.env]["database"], m[Rails.env]["user"], m[Rails.env]["pass"])
+    $connection.apply_saved_authentication()
+  end
+end
