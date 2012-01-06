baa71c2a5c9069d7b6fd80bb028d5027b85ee88b
Olivier BONNAURE <o.bonnaure@solisoft.net>
2012-01-06 12:45:51 +0100

    Layout & Page Creation (WIP)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,23 @@
 class HomeController < ApplicationController
   before_filter :need_auth
+  layout nil
   
   def index
     unless @db.collection("layouts").find_one().nil?
       # If there is no layout then create a new one
       l = {}
-      
+      l["name"] = "website"
+      l["html"] = "<!DOCTYPE html><html><head></head><body></body></html>"
+      l["js"] = [{ "jquery" => "" }, { "somethingelse" => "" }]
+      @db.collection("layouts").save(l)
+      # If there is no page then create a new one with default layout
+      page = {}
+      page["url"] = "/index.html"
+      page["metas"] = { :d => "Home page", :k => "home, page" }
+      # Save first page in Account model
+      @account["first_page"] = "/index.html"
+      @bdb.collection("accounts").save(@account)
     end
-    # If there is no page then create a new one with default layout
     
     # Then load page structure
     # And page data
